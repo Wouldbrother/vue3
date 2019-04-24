2bdfb313aa4e1d5409d3b49c2677df2bdec25ae4
cjh <975229470@qq.com>
2019-04-24 14:24:23 +0800

    vue3 sass引入

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,34 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div class="test">
+      阿三
+      <p>阿四</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="scss" scoped>
+$redcolor: red;
+$bluecolor: blue;
+.test {
+  color: $redcolor;
+  p {
+    color: $bluecolor;
+  }
+}
 </style>
+
+
+
+
